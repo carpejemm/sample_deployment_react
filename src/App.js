c7a7db8b90b4eb7526699a23b0e14a5f6dcedf9f
carpejemm <jerelydclaguda@gmail.com>
2022-11-14 23:42:30 +0800

    new porj

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { useState, useEffect } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
-  
+function App() {
   return (
-    <div className='App'>
-      <h1 className='app-title'>Monsters Rolodex</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jss</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
